3745458318b287f439fba2b7574047049f86da8e
VaibhavSingh98 <s.vaibhav1998@gmail.com>
2020-01-21 05:15:19 -0800

    first commit

---
+++ automate.sh
@@ -0,0 +1,44 @@
+username=`git config github.user`
+if [ "$username" = "" ]; then
+    echo "Could not find username, run 'git config --global github.user <username>'"
+    invalid_credentials=1
+fi
+
+
+dir_name=`basename $(pwd)`
+read -p "Do you want to use '$dir_name' as a repo name?(y/n)" answer_dirname
+case $answer_dirname in
+  y)
+    # use currently dir name as a repo name
+    reponame=$dir_name
+    ;;
+  n)
+    read -p "Enter your new repository name: " reponame
+    if [ "$reponame" = "" ]; then
+        reponame=$dir_name
+    fi
+    ;;
+  *)
+    ;;
+esac
+
+
+echo "Creating Github repository '$reponame' ..."
+curl -u $username https://api.github.com/user/repos -d '{"name":"'$reponame'"}'
+echo " done."
+
+
+echo "Creating README ..."
+touch README.md
+echo " done."
+
+
+echo "Pushing to remote ..."
+git init
+git add -A
+git commit -m "first commit"
+git remote rm origin
+git remote add origin https://github.com/$username/$reponame.git
+git push -u origin master
+echo " done."
+
